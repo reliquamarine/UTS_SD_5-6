d52a634d11a7fcc183221748aa97aa700343d3bd
Blessly Silaban <blesslysilaban@gmail.com>
2025-11-05 20:39:18 +0700

    soal3
    Update soal3

--- soal3.cpp
+++ soal3.cpp
@@ -1,1 +1,63 @@
-//Masukkan jawaban disini
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int V, E;
+    cout << "Jumlah vertex dan edge: ";
+    cin >> V >> E;
+    
+    vector<vector<int>> graph(V);
+    
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    
+    int S, K;
+    cin >> S >> K;
+    
+    vector<int> level(V, -1);
+    queue<int> q;
+    q.push(S);
+    level[S] = 0;
+    
+    while (!q.empty()) {
+        int curr = q.front();
+        q.pop();
+        
+        for (int neighbor : graph[curr]) {
+            if (level[neighbor] == -1) {
+                level[neighbor] = level[curr] + 1;
+                q.push(neighbor);
+            }
+        }
+    }
+    
+    cout << "Node Awal dan Hari Terinfeksi: " << S << " " << K << endl;
+    
+    vector<int> infected;
+    for (int i = 0; i < V; i++) {
+        if (level[i] == K) {
+            infected.push_back(i);
+        }
+    }
+    
+    cout << "Node terinfeksi: ";
+    if (infected.empty()) {
+        cout << "(TIDAK ADA)" << endl;
+    } else {
+        for (size_t i = 0; i < infected.size(); i++) {  // ubah jadi size_t
+            cout << infected[i];
+            if (i < infected.size() - 1) cout << " ";  
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
